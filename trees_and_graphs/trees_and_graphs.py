5a3fbd79696782b73fd745b79befb27b7e30bf36
Alec Garza-Galindo <alecgg@gmail.com>
2017-10-01 19:43:02 -0600

    Thinking about grapha and tree stuff

--- trees_and_graphs/trees_and_graphs.py
+++ trees_and_graphs/trees_and_graphs.py
@@ -1,16 +1,80 @@
+from collections import deque
+
+
+class Edge:
+    def __init__(self, source, dest, weight=1):
+        self.source = source
+        self.dest = dest
+        self.weight = weight
+
+
+class Node:
+    def __init__(self, edges=None):
+        self.edges = edges
+        if edges is None:
+            self.edges = []
+
+
+class DirectedGraph:
+    def __init__(self, nodes=None):
+        self.nodes = None
+        if nodes is None:
+            self.nodes = []
+
+
+class TreeNode:
+    pass
+
+
+class BinaryTree:
+    pass
+
+
 # 4.1 Given a directed graph, design an algorithm to find out whether there is a route between two nodes.
+def route_exists(node, other_node):
+    frontier = deque([node])
+    visited = set()
+    while len(frontier) > 0:
+        check = frontier.popleft()
+        if check is other_node:
+            return True
+        visited.add(check)
+        for edge in check.edges:
+            if edge.dest not in frontier and edge.dest not in visited:
+                frontier.append(edge.dest)
+    return False
+
 
 # 4.2 Minimal Tree: Given a sorted (increasing order) array with unique integer elements, write an
 # algorithm to create a binary search tree with minimal height.
+def binary_tree_from_sorted_list(sorted_list):
+    # make middle root
+    # split into L/R groups
+    # make middle of L group root of new tree on left
+    # make middle of R group root of new tree on right
+    # continue
+    pass
 
-# 4.3 List of Depths: GIven a binary tree, design an algorithm which creates a linked list of all the nodes
+
+# 4.3 List of Depths: Given a binary tree, design an algorithm which creates a linked list of all the nodes
 # at each depth (e.g., if you have a tree with depth D, you'll have D linked lists).
+def list_of_depths(binary_tree):
+    # initial list - add root
+    # add left and right to list?
+    pass
+
 
 # 4.4 Check Balanced: Implement a function to check if a binary tree is balanced.  For the purposes of
-# this question, a balanced tree is defined to be a tree such that the heights of the two subrees of any
+# this question, a balanced tree is defined to be a tree such that the heights of the two subtrees of any
 # node never differ by more than one.
+def is_balanced(binary_tree):
+    pass
+
 
 # 4.5 Validate BST: Implement a function to check if a binary tree is a binary search tree.
+def is_binary_search_tree(binary_tree):
+    pass
+
 
 # 4.6 Successor: Write an algorithm to find the "next" node (i.e., the in-order successor) of a given node in a
 # binary search tree.  You may assume that each node has a link to its parent.
