f981b0b62163144a2a0b7f6f811523f9d0a781fa
Alec Garza-Galindo <alecgg@gmail.com>
2017-10-04 09:13:01 -0600

    Starting bit manipulation exercises

--- bit_manipulation/bit_manipulation.py
+++ bit_manipulation/bit_manipulation.py
@@ -4,17 +4,41 @@
 # M = 10011, you can assume that there are at least 5 bits between j and i.  You would not, for
 # example, have j = 3 and i = 2, because M could not fully fit between bit 3 and bit 2.
 
-# 5.2 Binary to String: Given a real number between 0 and 1 (e.g., 0.72)that is passed in as a double, print
+
+def insert(n: int, m: int, i: int, j: int) -> int:
+    length = 32
+    # n     m   i   j
+    # 11111 00  1   3 ->  10001?
+
+    # Clear space in n for m of size j - i + 1 at positions [i, j] (part the sea)
+    # 1---1
+    # shift m over by i positions
+    # --00-
+    # insert m into n
+    # 1-001
+    pass
+
+
+# 5.2 Binary to String: Given a real number between 0 and 1 (e.g., 0.72) that is passed in as a double, print
 # the binary representation.  If the number cannot be represented accurately in binary with at most 32
 # characters, print "ERROR."
+def binary_to_string(number):
+    pass
+
 
 # 5.3 Flip Bit to Win: You have an integer and you can flip exactly one bit from a 0 to a 1.  Write code to
 # find the length of the longest sequence of 1s you could create.
+def longest_one_run(integer):
+    pass
+
 
 # 5.4 Next Number: Given a positive integer, print the next smallest and the next largest number that
 # have the same number of 1 bits in their binary representation.
+def next_number(integer):
+    pass
 
 # 5.5 Debugger: Explain what the following code does: ((n & (n - 1)) == 0)
+
 
 # 5.6 Conversion: Write a function to determine the number of bits you would need to flip to convert
 # integer A to integer B.
