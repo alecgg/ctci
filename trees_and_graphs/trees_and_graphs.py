e4cb18e6963ea541dec0a6f2d3ee3de243d14d19
Alec Garza-Galindo <alecgg@gmail.com>
2017-10-03 18:59:33 -0600

    More work...

--- trees_and_graphs/trees_and_graphs.py
+++ trees_and_graphs/trees_and_graphs.py
@@ -1,4 +1,5 @@
 from collections import deque
+from itertools import permutations
 
 
 class Edge:
@@ -175,33 +176,112 @@ def is_binary_search_tree(binary_tree: BinaryTree) -> bool:
 # 4.6 Successor: Write an algorithm to find the "next" node (i.e., the in-order successor) of a given node in a
 # binary search tree.  You may assume that each node has a link to its parent.
 def successor(node: TreeNode) -> TreeNode:
-    pass
+    # if you have a right child, then it's the left-most child on the right-side (smallest thing greater than you)
+    # if you have no right child, then it's the earliest parent that is greater than you
+    # you are the end of a in-order traversal of a left-subtree
+    # so go back up to the node who's left-side has been explored
+    if node.right is None:
+        parent = node.parent
+        while parent is not None:
+            if node.value <= parent.value:
+                return parent
+            parent = parent.parent
+        return parent
+    child = node.right
+    while child.left is not None:
+        child = child.left
+    return child
+
 
 # 4.7 Build Order: You are given a list of projects and a list of dependencies (which is a list of pairs of
 # projects, where the second project is dependent on the first project).  ALl of a project's dependencies
 # must be built before the project is.  Find a build order that will allow the projects to be built.  If there
 # is no valid build order, return an error.
+def build_order(projects, dependencies):
+    # find projects with no dependencies
+    # add projects to initial list
+    # remove dependencies on those projects
+    # find projects with no dependencies...
+    # repeat...
+    projects = projects[:]
+    dependencies = dependencies[:]
+
+    dependees = [d[1] for d in dependencies]
+    no_dependencies_projects = [p for p in projects if p not in dependees]
+    if len(no_dependencies_projects) == 0:
+        return 'ERROR'
+
+    build = no_dependencies_projects[:]
+    while len(no_dependencies_projects) > 0:
+        projects = [p for p in projects if p not in build]
+        dependencies = [d for d in dependencies if d[0] not in build]
+        dependees = [d[1]for d in dependencies]
+        no_dependencies_projects = [p for p in projects if p not in dependees]
+        build = [p for p in no_dependencies_projects if p not in build]
+
+    if len(projects) != 0:
+        return 'ERROR'
+    return build
+
 
 # 4.8 First Common Ancestor: Design an algorithm and write code to find the first common ancestor
 # of two nodes in a binary tree.  Avoid storing additional nodes in a data structure.  NOTE: This is not
 # necessarily a binary search tree.
+def first_common_ancestor(node, other_node):
+    # do like a nested for loop.  start at node and other node
+    # check if going up other node's ancestors meets node
+    # if not, update node to node.parent and try again
+    ancestor = node
+    while ancestor is not None:
+        check_node = other_node
+        while check_node is not None:
+            if ancestor is check_node:
+                return ancestor
+            check_node = check_node.parent
+        ancestor = ancestor.parent
+    return ancestor
+
 
 # 4.9 BST Sequences: A binary search tree was created by traversing through an array from left to right
 # and inserting each element.  Given a binary search tree with distinct elements, print all possible
 # arrays that could have led to this tree.
+def bst_sequences(binary_search_tree):
+    # make array out of BST
+    # generate all permutations of array
+    array = []
+    to_visit = deque([binary_search_tree.root])
+    while len(to_visit) > 0:
+        node = to_visit.popleft()
+        if node is None:
+            continue
+        array.append(node.value)
+        to_visit.extend([node.left, node.right])
+    return permutations(array)
+
 
 # 4.10  Check Subtree; T1 and T2 are two very large binary trees, with T1 much bigger than T2.  Create an
 # algorithm to determine if T2 is a subtree of T1.
 # A tree T2 is a subtree of T1 if there exists a node n in T1 such that the subtree of n is identical to T2.
 # That is, if you cut off the tree at node n, the two trees would be identical.
+def is_subtree(bigger_tree, smaller_tree):
+    # look for the root of the smaller tree in the bigger tree
+    pass
+
 
 # 4.11 Random Node: You are implementing a binary tree class from scratch which, in addition to
 # insert, find, and delete, has a method getRandomNode() which returns a random node
 # from the tree.  ALl nodes should be equally likely to be chosen.  Design and implement an algorithm
 # for getRandomNode, and explain how you would implement the rest of the methods.
+def get_random_node(binary_tree):
+    # flatten array into an array and pick random element
+    # pointer to root
+    pass
+
 
 # 4.12 Paths with Sum: You are given a binary tree in which each node contains an integer value (which
 # might be a positive or negative).  Design an algorithm to count the number of paths that sum to a
 # given value.  The path does not need to start or end at the root or a leaf, but it must go downwards
 # (traveling only from parent nodes to child nodes).
-
+def path_sum(binary_tree, value):
+    # each node has a sum value, do an in-order traversal
+    pass
