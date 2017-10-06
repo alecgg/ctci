3ddeabd29a975729d1df567d0829fdf7e1b2fcb5
Alec Garza-Galindo <alecgg@gmail.com>
2017-10-06 09:22:56 -0600

    Binary to string and run of 1s problem

--- bit_manipulation/bit_manipulation.py
+++ bit_manipulation/bit_manipulation.py
@@ -28,22 +28,53 @@ def insert(n: int, m: int, i: int, j: int) -> int:
 # 5.2 Binary to String: Given a real number between 0 and 1 (e.g., 0.72) that is passed in as a double, print
 # the binary representation.  If the number cannot be represented accurately in binary with at most 32
 # characters, print "ERROR."
-def binary_to_string(number):
+def binary_to_string(number: float) -> str:
     # OK so 0.72 to 0.0101010101
     # read that incorrectly
     # 0.72 = (7 * 10^-1) + (2 * 10^-2) + (0 * 10^-3) + ...
-    pass
+
+    # 9 = 9 * 10^0
+    # 9 = 1001 <- what's the algorithm?
+    # subtract max power of two < number
+    # add 1 to digit
+    # repeat
+    string = ['0', '.']
+    for power in range(-1, -33, -1):
+        digit = '0'
+        if number - 2**power >= 0.0:
+            digit = '1'
+            number -= 2**power
+        string.append(digit)
+    if number > 0.0:
+        return 'ERROR'
+    return ''.join(string)
 
 
 # 5.3 Flip Bit to Win: You have an integer and you can flip exactly one bit from a 0 to a 1.  Write code to
 # find the length of the longest sequence of 1s you could create.
-def longest_one_run(integer):
-    pass
+def longest_one_run(integer: int) -> int:
+    # length if 0 surrounded by 1s = left run + 1 + right run
+    # length if 0 has 1 on left = left run + 1
+    # length if 0 has 1 on right = 1 + right run
+    maximum = 0
+    with_swap = 0
+    without_swap = 0
+    for i in range(32):
+        last_digit = integer & 0b1
+        integer >>= 1
+        if last_digit is 1:
+            with_swap += 1
+            without_swap += 1
+        elif last_digit is 0:
+            with_swap = without_swap + 1
+            without_swap = 0
+        maximum = max([maximum, with_swap, without_swap])
+    return maximum
 
 
 # 5.4 Next Number: Given a positive integer, print the next smallest and the next largest number that
 # have the same number of 1 bits in their binary representation.
-def next_number(integer):
+def next_number(integer: int) -> (int, int):
     pass
 
 # 5.5 Debugger: Explain what the following code does: ((n & (n - 1)) == 0)
@@ -51,6 +82,8 @@ def next_number(integer):
 
 # 5.6 Conversion: Write a function to determine the number of bits you would need to flip to convert
 # integer A to integer B.
+def num_flips(a: int, b: int) -> int:
+    pass
 
 # 5.7 Pairwise swap.  Write a program to swap odd and even bits in an integer with as few instructions as
 # possible.  (e.g., bit 0 and bit 1 are swapped, bit 2 and bit 3 are swapped, and so on).
