186c4470e97e25512e4af2d22d8e80deb8d26fd0
Alec Garza-Galindo <alecgg@gmail.com>
2017-10-11 19:46:39 -0600

    Robots!

--- recursion_dynamic_programming/recursion_dynamic_programming.py
+++ recursion_dynamic_programming/recursion_dynamic_programming.py
@@ -1,15 +1,68 @@
+from typing import List
+
 # 8.1 Triple Step: A child is running up a staircase with n steps and can hop either 1 step, 2 steps, or 3
 # steps at a time.  Implement a method to count how many possible ways the child can run up the
 # stairs.
+def step_paths(num_steps: int) -> int:
+    num_paths = 0
+    if num_steps > 1:
+        num_paths += 1 + step_paths(num_steps - 1)
+    if num_steps > 2:
+        num_paths += 1 + step_paths(num_steps - 2)
+    if num_steps > 3:
+        num_paths += 1 + step_paths(num_steps - 3)
+    return num_steps
+
 
 # 8.2 Robot in a Grid: Imagine a robot sitting on the upper left corner of a grid with r rows and c columns.
 # The robot can only move in two directions, right and down, but certain cells are "off limits" such that
 # the robot cannot step on them.  Design an algorithm to find a path for the robot from the top left to
 # the bottom right.
+def top_left_to_bottom_right(grid):
+    num_rows = len(grid)
+    num_cols = len(grid[0])
+    goal = (num_rows - 1, num_cols - 1)
+    path = top_left_to_bottom_right_helper(grid, [(0, 0)])
+    if path is not None and path[-1] == goal:
+        return path
+    return None
+
+
+def top_left_to_bottom_right_helper(grid, path):
+    num_rows = len(grid)
+    num_cols = len(grid[0])
+    goal = (num_rows - 1, num_cols - 1)
+    row, col = path[-1]
+
+    # Reached goal
+    if (row, col) == goal:
+        return path + [(row, col)]
+
+    # Reached off grid or no-no square
+    if row >= num_rows or col >= num_cols or not grid[row][col]:
+        return path[:]
+
+    # Go right
+    right = top_left_to_bottom_right_helper(grid, path + [(row + 1, col)])
+
+    # Go down
+    down = top_left_to_bottom_right_helper(grid, path + [(row, col + 1)])
+
+    if right is not None and right[-1] == goal:
+        return right
+
+    if down is not None and down[-1] == goal:
+        return down
+
+    return None
+
 
 # 8.3 Magic Index: A magic index in an array A[0...n-1] is defined to be an index such that A[i] =
 # i.  Given a sorted array of distinct integers, write a method to find a magic index, if one exists, in
 # array A.
+def magic_index(array):
+    # array of 0..n-1 compared to 
+    pass
 
 # 8.4 Power Set: Write a method to return all subsets of a set.
 
