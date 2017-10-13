50c0936df2c75094c510568d86787b3500f848e6
Alec Garza-Galindo <alecgg@gmail.com>
2017-10-12 19:44:26 -0600

    Some more recursion and dynamic programming exercises

--- recursion_dynamic_programming/recursion_dynamic_programming.py
+++ recursion_dynamic_programming/recursion_dynamic_programming.py
@@ -1,5 +1,3 @@
-from typing import List
-
 # 8.1 Triple Step: A child is running up a staircase with n steps and can hop either 1 step, 2 steps, or 3
 # steps at a time.  Implement a method to count how many possible ways the child can run up the
 # stairs.
@@ -28,6 +26,7 @@ def top_left_to_bottom_right(grid):
     return None
 
 
+# could memoize
 def top_left_to_bottom_right_helper(grid, path):
     num_rows = len(grid)
     num_cols = len(grid[0])
@@ -61,14 +60,39 @@ def top_left_to_bottom_right_helper(grid, path):
 # i.  Given a sorted array of distinct integers, write a method to find a magic index, if one exists, in
 # array A.
 def magic_index(array):
-    # array of 0..n-1 compared to 
-    pass
+    return magic_index_helper(array, 0, len(array) - 1)
+
+
+def magic_index_helper(array, left, right):
+    if left > right:
+        return None
+    mid = (left + right) // 2
+    if array[mid] == mid:
+        return mid
+    if mid < array[mid]:
+        return magic_index_helper(array, left, mid - 1)
+    if mid > array[mid]:
+        return magic_index_helper(array, mid + 1, right)
+
 
 # 8.4 Power Set: Write a method to return all subsets of a set.
+def power_set(elements):
+    power_set_of_elements = [[]]
+    for element in elements:
+        new_sets = []
+        for set_ in power_set_of_elements:
+            new_sets.append(set_ + [element])
+        power_set_of_elements.extend(new_sets)
+    return power_set_of_elements
+
 
 # 8.5 Recursive Multiply: Write a recursive function to multiply two positive integers without using the
 # * operator.  You can use addition, subtraction, and bit shifting, but you should minimize the
 # number of those operations.
+def multiply(a, b):
+    output = 0
+    pass
+
 
 # 8.6 Towers of Hanoi: In the classic problem of the Towers of Hanoi, you have 3 towers and N disks of
 # different sizes which can slide onto any tower.  The puzzle starts with disks sorted in ascending order
